d3b74ae034a78b3c52d6cf2df94e18e3a35a728f
CodeMaster4711 <cedi27@t-online.de>
2025-10-26 13:03:45 +0100

    feat: added backend to docker compose

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
 use dotenvy::dotenv;
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{Database};
 use std::env;
+use migration::{Migrator, MigratorTrait};
 
 #[tokio::main]
 async fn main() {
@@ -8,4 +9,7 @@ async fn main() {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let conn = Database::connect(&db_url).await.unwrap();
     println!("Database connected");
+
+    Migrator::up(&conn, None).await.unwrap();
+    println!("Migrations applied");
 }
