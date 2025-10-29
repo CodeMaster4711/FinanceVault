8b63b50c5980a8367b720611b082431cb93e5019
CodeMaster4711 <cedi27@t-online.de>
2025-10-29 20:29:23 +0100

    feat: added expenses tabs

---
+++ frontend/src/routes/expenses/monthly/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import * as Tabs from "$lib/components/ui/tabs/index.js";
+</script>
+
+<div class="h-screen w-full">
+  <Tabs.Root value="overview" class="w-full h-full">
+    <Tabs.List>
+      <Tabs.Trigger value="overview">Overview</Tabs.Trigger>
+      <Tabs.Trigger value="expenses">Expenses</Tabs.Trigger>
+      <Tabs.Trigger value="subscription">Subscription</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="overview">
+      Make changes to your account here.
+    </Tabs.Content>
+    <Tabs.Content value="expenses">Change your expenses here.</Tabs.Content>
+    <Tabs.Content value="subscription"
+      >Manage your subscription here.</Tabs.Content
+    >
+  </Tabs.Root>
+</div>
