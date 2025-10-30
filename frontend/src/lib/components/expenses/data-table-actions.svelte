a6ccdc090ce5d8c34d2e21c758c30ad27c6b78e3
CodeMaster4711 <cedi27@t-online.de>
2025-10-30 19:57:32 +0100

    feat: added monthly expenses ui

---
+++ frontend/src/lib/components/expenses/data-table-actions.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { MoreHorizontal, Pencil, Trash } from "@lucide/svelte";
+
+  type Props = {
+    onEdit: () => void;
+    onDelete: () => void;
+  };
+
+  let { onEdit, onDelete }: Props = $props();
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    {#snippet child({ props })}
+      <Button variant="ghost" size="sm" class="h-8 w-8 p-0" {...props}>
+        <span class="sr-only">Menü öffnen</span>
+        <MoreHorizontal class="h-4 w-4" />
+      </Button>
+    {/snippet}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content align="end">
+    <DropdownMenu.Item onclick={onEdit}>
+      <Pencil class="mr-2 h-4 w-4" />
+      Bearbeiten
+    </DropdownMenu.Item>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item onclick={onDelete} class="text-destructive">
+      <Trash class="mr-2 h-4 w-4" />
+      Löschen
+    </DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -67,19 +67,6 @@
             orientation="vertical"
             class="mr-2 data-[orientation=vertical]:h-4"
           />
-          <Breadcrumb.Root>
-            <Breadcrumb.List>
-              <Breadcrumb.Item class="hidden md:block">
-                <Breadcrumb.Link href="##"
-                  >Building Your Application</Breadcrumb.Link
-                >
-              </Breadcrumb.Item>
-              <Breadcrumb.Separator class="hidden md:block" />
-              <Breadcrumb.Item>
-                <Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-              </Breadcrumb.Item>
-            </Breadcrumb.List>
-          </Breadcrumb.Root>
         </div>
       </header>
       <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
